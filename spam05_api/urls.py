a9e9f9707ac92e0f3b82cb797d1ffe7cfd946505
mcomanescu <mery.comanescu@gmail.com>
2014-05-23 17:48:09 -0600

    first commit

---
+++ spam05_api/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# from django.contrib import admin
+# admin.autodiscover()
+
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+urlpatterns = patterns('',
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
