53a77c3fb28d153f73638bc51b115972ff372987
mcomanescu <mery.comanescu@gmail.com>
2014-07-04 09:28:23 -0400

    update

--- spam_import.py
+++ spam_import.py
@@ -29,7 +29,7 @@ url = 'http://127.0.0.1:8000/area/'
 # , 'RWA', 'BDI', 'SYC', 'AGO', 'ZMB', 'MWI', 'MUS', 'MOZ', 'MDG', 'ZWE', 'NAM', 'BWA'
 # , 'ZAF', 'SWZ', 'LSO', 'USA']
 # for cnt in p['iso3'].unique():
-for cnt in ['BTN', 'BGD', 'KHM', 'IDN', 'BRN', 'MNG', 'JPN', 'PRK', 'KOR', 'PAK', 'NPL', 'MMR', 'VNM', 'LAO', 'PHL', 'THA', 'LKA', 'MYS', 'MDV', 'SGP', 'TLS', 'CAN', 'NOR', 'FIN', 'SWE', 'GBR', 'EST', 'LVA', 'DNK', 'LTU', 'BLR', 'KAZ', 'IRL', 'DEU', 'POL', 'NLD', 'UKR', 'BEL', 'FRA', 'CZE', 'LUX', 'SVK', 'AUT', 'HUN', 'MDA', 'ROU', 'CHE', 'ITA', 'SVN', 'HRV', 'SRB', 'BIH', 'UZB', 'BGR', 'ESP', 'MNE', 'GEO']:
+for cnt in ['KGZ', 'TKM', 'ALB', 'MKD', 'PRT', 'TUR', 'AZE', 'GRC', 'ARM', 'TJK', 'CYP', 'MEX' , 'CUB', 'HTI', 'DOM', 'JAM', 'PRI', 'BLZ', 'GLP', 'GTM', 'VIR', 'ATG', 'KNA', 'MSR' , 'HND', 'DMA', 'NIC', 'MTQ', 'SLV', 'LCA', 'VCT', 'BRB', 'COL', 'GRD', 'VEN', 'TTO' , 'CRI', 'PAN', 'GUY', 'SUR', 'GUF', 'BRA', 'ECU', 'PER', 'BOL', 'CHL', 'PRY', 'ARG' , 'URY', 'IRN', 'IRQ', 'SYR', 'MLT', 'LBN', 'JOR', 'ISR', 'SAU', 'KWT', 'OMN', 'BHR' , 'QAT', 'ARE', 'YEM', 'TUN', 'DZA', 'MAR', 'LBY', 'EGY', 'KIR', 'PNG', 'SLB', 'AUS' , 'VUT', 'FJI', 'NCL', 'NZL', 'RUS', 'MLI', 'NER', 'TCD', 'SDN', 'MRT', 'ERI', 'SEN' , 'BFA', 'ETH', 'GMB', 'NGA', 'CMR', 'DJI', 'GNB', 'GIN', 'BEN', 'SOM', 'GHA', 'TGO' , 'CAF', 'CIV', 'SLE', 'LBR', 'COD', 'KEN', 'GNQ', 'UGA', 'COG', 'GAB', 'STP', 'TZA' , 'RWA', 'BDI', 'SYC', 'AGO', 'ZMB', 'MWI', 'MUS', 'MOZ', 'MDG', 'ZWE', 'NAM', 'BWA' , 'ZAF', 'SWZ', 'LSO', 'USA']:
 	print cnt
 	p_cnt = p[p['iso3'] == str(cnt)].to_json(orient = 'records')
 	tt = json.loads(p_cnt)
@@ -38,7 +38,6 @@ for cnt in ['BTN', 'BGD', 'KHM', 'IDN', 'BRN', 'MNG', 'JPN', 'PRK', 'KOR', 'PAK'
 		req = ul2.Request(url, data)
 		response = ul2.urlopen(req)
 		the_page = response.read();
-
 
 # single tests
 # p_cnt = p[p['iso3'] == 'AFG'].to_json(orient = 'records')
@@ -50,3 +49,7 @@ for cnt in ['BTN', 'BGD', 'KHM', 'IDN', 'BRN', 'MNG', 'JPN', 'PRK', 'KOR', 'PAK'
 # response = ul2.urlopen(req)
 # the_page = response.read();
 # print data
+
+# stats = p.groupby('iso3').size()
+print stats
+print stats['CAN']
