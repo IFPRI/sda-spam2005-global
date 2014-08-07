9d5de7443be7ee644cfbe4f024dbda0461a856d1
mcomanescu <mery.comanescu@gmail.com>
2014-08-07 18:43:59 -0400

    updates

---
+++ javascripts/data.js
@@ -0,0 +1,117 @@
+console.log('This would be the main JS file.');
+
+jQuery(document).ready(function( $ ) {
+	$('.btn').button();
+	// Retrieve country list
+	$.getJSON("countries.json", function(data) {
+		$.each(data, function(i, item) {
+			var newOption = $('<option/>');
+			newOption.html(item['adm0_name']);
+			newOption.attr('value', item['iso3']);
+			$("#countries").append(newOption);
+		});
+
+		$("#countries").select2({
+			placeholder: "Select one or more countries",
+			allowClear: true,
+		});
+		$("#crops").select2({
+			placeholder: "Select crop(s)",
+			allowClear: true,
+		});
+		$("#technologies").select2({
+			header: "Select technologies",
+			allowClear: true,
+		});
+		$("#file_format").select2({
+			header: "Select a format",
+			allowClear: true,
+		});
+	});
+
+	// Download data
+	$("#download_button").click(function() {
+	variable = 'yield';
+	if (jQuery("#yield").is(":checked") == true) {
+		variable = 'yield';
+	}
+    	if (jQuery("#production").is(":checked") == true) {
+		variable = 'prod';
+    	}
+    	if (jQuery("#harvested_area").is(":checked") == true) {
+		variable = 'harvested';
+    	}
+    	if (jQuery("#value_of_production").is(":checked") == true) {
+		variable = 'vop';
+    	}
+	// uncomment this when ready to download whole countries
+	variable = variable + '_all';
+    	
+    	var format = "csv";
+   	if ($('#file_format').val()) {
+   		format = $('#file_format').val();
+   	}
+        var crops = [];
+        var cropsSelected = $('#crops option:selected');
+	
+	var techSelected = $('#technologies option:selected');
+	jQuery.each(techSelected, function(i, item) {
+		crop_suffix = $(item).attr('value');
+        	$.each(cropsSelected, function(i, item) {
+			crops.push($(item).attr('value') + crop_suffix);
+		})
+        })
+
+        if (crops.length == 0) {
+            alert("Select a crop");
+            return false;
+        }
+
+        var fields = "iso3,x,y,prod_level,unit,cell5m,rec_type," + crops.join(',');
+
+    	var countries = $('#countries').val();
+    	if (jQuery('#countries').val()) {
+    		var countries = $('#countries').val().join(",");
+    	} else {
+    		alert("Select a country");
+    		return false;
+    	}
+
+	//alert(countries);
+	//alert(fields);
+	//alert(variable);
+
+        var date = Date.now();
+    	var filename = 'spam2005_' + date;
+	params = {
+		//page: 1, // comment this when ready to download whole countries
+		// api_key: "4e78c23fe0c7a1263a87cc9261df1bca228daac0",
+		iso3: countries,
+		fields: fields,
+		format: format,
+		// filename: filename,
+	}
+	var url = "http://api.mapspam.info/" + variable + "/?" + $.param(params);
+    	//alert(url);
+	console.log(url);
+    	window.open(url, "_blank");
+
+        // download metadata file - description of the downloaded variables
+        /* Jawoo said not to download metadata file for now
+        var dbQueryMeta = "SELECT varcode, unit, varlabel, vartitle, vardesc, year, version, cat1, cat2, aggtype, aggfun, dauthor, citation, sources FROM vi where varcode in ('" +  crops_meta.join("', '") + "') and published = '1' and tbname = '" + tableName +"'";
+        var filenameMeta = filename + '_meta';
+        //alert(dbQueryMeta);
+        paramsMeta = {
+            page: 1,
+            // api_key: "4e78c23fe0c7a1263a87cc9261df1bca228daac0",
+            fields: fields,
+            format: "csv",
+        }
+	var meta_url = "http://api.mapspam.info/yield/?" + + "?" +  $.param(paramsMeta);
+        window.open(meta_url, "_blank");*/
+
+  	});
+});
+//http://api.mapspam.info/yield/?page=1&iso3=MWI,GHA&fields=iso3,x,y,prod_level,cell5m,unit,whea&format=csv
+
+// http://harvestchoice.cartodb.com/api/v2/sql?q=select%20varcode,%20varlabel,%20published,%20tbname%20from%20vi%20where%20position(%27cell5m_spam%27%20in%20tbname)%20=0%20and%20published%20=%20%271%27%20order%20by%20varlabel&api_key=4e78c23fe0c7a1263a87cc9261df1bca228daac0
