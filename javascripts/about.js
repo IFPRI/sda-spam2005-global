452c4d37680cc35e54368144f0fbb1cd75c4e243
mcomanescu <mery.comanescu@gmail.com>
2014-08-07 22:23:18 -0400

    updates

--- javascripts/about.js
+++ javascripts/about.js
@@ -1,10 +1,33 @@
-jQuery(document).ready(function() {
- jQuery('input.prettyCheckable').each(function(){
-  $(this).prettyCheckable();
- });
-$("#download_button").click(function() {
-  jQuery('input.prettyCheckable').each(function(){
-  alert($(this))
-  });
-});
+$(document).ready(function() {
+	
+	$('.btn').button();
+
+	$('input.prettyCheckable').each(function(){
+		$(this).prettyCheckable();
+	});
+
+	$("#download_button").click(function() {
+
+		$('input.prettyCheckable').each(function(){
+			alert($(this))
+		});
+
+		variable = 'yield';
+		if ($("#yield").is(":checked") == true) {
+			variable = 'yield';
+		}
+    	
+    	if ($("#production").is(":checked") == true) {
+			variable = 'production';
+    	}
+    	
+    	if ($("#harvested_area").is(":checked") == true) {
+			variable = 'harvested_area';
+    	}
+
+    	if ($("#physical_area").is(":checked") == true) {
+			variable = 'physical_area';
+    	}
+
+	});
 });
