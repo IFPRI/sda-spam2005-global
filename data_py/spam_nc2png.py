2a30fe8acd6a806e304aa859961c92ae240de184
mcomanescu <mery.comanescu@gmail.com>
2014-08-18 21:12:07 -0400

    updates

--- data_py/spam_nc2png.py
+++ data_py/spam_nc2png.py
@@ -48,13 +48,13 @@ def plotCrop(variableFolder, crop):
 		bmap = sequential.YlOrBr[bins]; unitLabel = 'ha';
 		parentFolder = 'quickstart_harvested'; variableName = 'Harvested Area';
 	if 'area' in variableFolder: 
-		bmap = sequential.Oranges[bins]; unitLabel = 'ha';
+		bmap = sequential.YlOrBr[bins]; unitLabel = 'ha';
 		parentFolder = 'quickstart_area'; variableName = 'Physical Area';
 	if 'yield' in variableFolder: 
 		bmap = sequential.YlGnBu[bins]; unitLabel = 'kg/ha';
 		parentFolder = 'quickstart_yield'; variableName = 'Yield';
 	if 'prod' in variableFolder: 
-		bmap = sequential.Greens[bins]; unitLabel = 'mt';
+		bmap = sequential.YlGn[bins]; unitLabel = 'mt';
 		parentFolder = 'quickstart_prod'; variableName = 'Production';
 
 	cropArr = crop.split('_');
@@ -104,5 +104,5 @@ def plotCrop(variableFolder, crop):
 	plt.savefig('/home/tmp/png/' + parentFolder + '/' + crop + '.png', format='png', dpi=400)
 
 for variableFolder in ('quickstart_harvested/', 'quickstart_area/', 'quickstart_yield/', 'quickstart_prod/'):
-	for crop in ('whea', 'rice', 'maiz', 'barl', 'whea_i', 'rice_i', 'maiz_i', 'barl_i', 'whea_r', 'rice_r', 'maiz_r', 'barl_r'):
+	for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest', 'whea_i', 'rice_i', 'maiz_i', 'barl_i', 'pmil_i', 'smil_i', 'sorg_i', 'ocer_i', 'pota_i', 'swpo_i', 'yams_i', 'cass_i', 'orts_i', 'bean_i', 'chic_i', 'cowp_i', 'pige_i', 'lent_i', 'opul_i', 'soyb_i', 'grou_i', 'cnut_i', 'oilp_i', 'sunf_i', 'rape_i', 'sesa_i', 'ooil_i', 'sugc_i', 'sugb_i', 'cott_i', 'ofib_i', 'acof_i', 'rcof_i', 'coco_i', 'teas_i', 'toba_i', 'bana_i', 'plnt_i', 'trof_i', 'temf_i', 'vege_i', 'rest_i', 'whea_r', 'rice_r', 'maiz_r', 'barl_r', 'pmil_r', 'smil_r', 'sorg_r', 'ocer_r', 'pota_r', 'swpo_r', 'yams_r', 'cass_r', 'orts_r', 'bean_r', 'chic_r', 'cowp_r', 'pige_r', 'lent_r', 'opul_r', 'soyb_r', 'grou_r', 'cnut_r', 'oilp_r', 'sunf_r', 'rape_r', 'sesa_r', 'ooil_r', 'sugc_r', 'sugb_r', 'cott_r', 'ofib_r', 'acof_r', 'rcof_r', 'coco_r', 'teas_r', 'toba_r', 'bana_r', 'plnt_r', 'trof_r', 'temf_r', 'vege_r', 'rest_r'):
 		plotCrop(variableFolder, crop)
