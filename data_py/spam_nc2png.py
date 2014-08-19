f12a98c1987820e10ae3911cfe52dee7ad11d4f8
mcomanescu <mery.comanescu@gmail.com>
2014-08-19 10:30:06 -0400

    updates

--- data_py/spam_nc2png.py
+++ data_py/spam_nc2png.py
@@ -1,7 +1,6 @@
 import matplotlib
 matplotlib.use('Agg')
 from mpl_toolkits.basemap import Basemap
-from netCDF4 import *
 from matplotlib.colors import BoundaryNorm, Normalize
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +10,7 @@ import pandas as pd
 from PIL import Image
 from netCDF4 import Dataset
 import numpy.ma as ma
+import pysal.esda.mapclassify as br
 
 variablePath  = '/home/tmp/nc/'
 
@@ -18,6 +18,7 @@ variablePath  = '/home/tmp/nc/'
 cropList = pd.read_csv('/home/django/spam2005-global/data_py/spam_crops.csv')
 
 def plotCrop(variableFolder, crop):
+	print crop
 	filename = crop + '.tiff.nc'
 	datafile = Dataset(variablePath + variableFolder + filename)
 
@@ -27,22 +28,19 @@ def plotCrop(variableFolder, crop):
 
 	d2 = ma.masked_where(data <= 1, data)
 	df = pd.DataFrame(d2)
-	df = np.round(df,0)
+
+	s = pd.Series()
+	for col in df.columns:
+    	s = s.append(pd.Series(df[col]))
+
+	s_one = s[~np.isnan(s)]
 	
-	bins = 7
-	success = False
+	jc = br.Jenks_Caspall(s_one, k = 8)
 
-	while not success:
-	    try:
-	        cat = pd.qcut(df, bins, retbins=True, precision = 1)
-	        success = True
-	    except ValueError:
-	    	bins = bins - 1
-	    	pass
-
-	for i in range(0,len(cat[1])):
-		if len(str(int(cat[1][i]))) >= 3: 
-			cat[1][i] = np.round(cat[1][i] * 0.01,0) / 0.01
+	bins = []
+	for i in range(0,len(jc.bins)):
+		if len(str(int(jc.bins[i]))) >= 3: 
+			bins[i] = np.round(jc.bins[i] / 50.0 ,0) * 50
 
 	if 'harvested' in variableFolder: 
 		bmap = sequential.YlOrBr[bins]; unitLabel = 'ha';
@@ -84,7 +82,7 @@ def plotCrop(variableFolder, crop):
 
 	map = Basemap(projection='merc',resolution='l', epsg=4326, lat_0 = 0, lon_0 = 20)
 	#map = Basemap(projection='merc',resolution='i', epsg=4326, llcrnrlon=-130, llcrnrlat=-75, urcrnrlon=185, urcrnrlat=80)
-	cs = map.pcolormesh(lons, lats, d2, cmap=cmap, norm=BoundaryNorm(cat[1], 256, clip=True))
+	cs = map.pcolormesh(lons, lats, d2, cmap=cmap, norm=BoundaryNorm(bins, 256, clip=True))
 	map.drawlsmask(land_color='#f4f4f4')
 	# cs = map.pcolormesh(lons, lats, data, cmap=cmap, norm=Normalize(vmin=data.min(), vmax=data.max(), clip=True))
 
@@ -104,5 +102,6 @@ def plotCrop(variableFolder, crop):
 	plt.savefig('/home/tmp/png/' + parentFolder + '/' + crop + '.png', format='png', dpi=400)
 
 for variableFolder in ('quickstart_harvested/', 'quickstart_area/', 'quickstart_yield/', 'quickstart_prod/'):
-	for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest', 'whea_i', 'rice_i', 'maiz_i', 'barl_i', 'pmil_i', 'smil_i', 'sorg_i', 'ocer_i', 'pota_i', 'swpo_i', 'yams_i', 'cass_i', 'orts_i', 'bean_i', 'chic_i', 'cowp_i', 'pige_i', 'lent_i', 'opul_i', 'soyb_i', 'grou_i', 'cnut_i', 'oilp_i', 'sunf_i', 'rape_i', 'sesa_i', 'ooil_i', 'sugc_i', 'sugb_i', 'cott_i', 'ofib_i', 'acof_i', 'rcof_i', 'coco_i', 'teas_i', 'toba_i', 'bana_i', 'plnt_i', 'trof_i', 'temf_i', 'vege_i', 'rest_i', 'whea_r', 'rice_r', 'maiz_r', 'barl_r', 'pmil_r', 'smil_r', 'sorg_r', 'ocer_r', 'pota_r', 'swpo_r', 'yams_r', 'cass_r', 'orts_r', 'bean_r', 'chic_r', 'cowp_r', 'pige_r', 'lent_r', 'opul_r', 'soyb_r', 'grou_r', 'cnut_r', 'oilp_r', 'sunf_r', 'rape_r', 'sesa_r', 'ooil_r', 'sugc_r', 'sugb_r', 'cott_r', 'ofib_r', 'acof_r', 'rcof_r', 'coco_r', 'teas_r', 'toba_r', 'bana_r', 'plnt_r', 'trof_r', 'temf_r', 'vege_r', 'rest_r'):
+	#for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest', 'whea_i', 'rice_i', 'maiz_i', 'barl_i', 'pmil_i', 'smil_i', 'sorg_i', 'ocer_i', 'pota_i', 'swpo_i', 'yams_i', 'cass_i', 'orts_i', 'bean_i', 'chic_i', 'cowp_i', 'pige_i', 'lent_i', 'opul_i', 'soyb_i', 'grou_i', 'cnut_i', 'oilp_i', 'sunf_i', 'rape_i', 'sesa_i', 'ooil_i', 'sugc_i', 'sugb_i', 'cott_i', 'ofib_i', 'acof_i', 'rcof_i', 'coco_i', 'teas_i', 'toba_i', 'bana_i', 'plnt_i', 'trof_i', 'temf_i', 'vege_i', 'rest_i', 'whea_r', 'rice_r', 'maiz_r', 'barl_r', 'pmil_r', 'smil_r', 'sorg_r', 'ocer_r', 'pota_r', 'swpo_r', 'yams_r', 'cass_r', 'orts_r', 'bean_r', 'chic_r', 'cowp_r', 'pige_r', 'lent_r', 'opul_r', 'soyb_r', 'grou_r', 'cnut_r', 'oilp_r', 'sunf_r', 'rape_r', 'sesa_r', 'ooil_r', 'sugc_r', 'sugb_r', 'cott_r', 'ofib_r', 'acof_r', 'rcof_r', 'coco_r', 'teas_r', 'toba_r', 'bana_r', 'plnt_r', 'trof_r', 'temf_r', 'vege_r', 'rest_r'):
+	for crop in ('whea', 'rice', 'maiz', 'whea_i', 'rice_i', 'maiz_i', 'whea_r', 'rice_r', 'maiz_r'):
 		plotCrop(variableFolder, crop)
