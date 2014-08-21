622d51a02ac848d6e6f31afd071e340dd64bced7
mcomanescu <mery.comanescu@gmail.com>
2014-08-21 12:24:05 -0400

    v2

---
+++ data_py/spam_nc2png_v2.py
@@ -0,0 +1,148 @@
+import matplotlib
+matplotlib.use('Agg')
+from mpl_toolkits.basemap import Basemap
+from matplotlib.colors import BoundaryNorm, Normalize
+import matplotlib.pyplot as plt
+import numpy as np
+import gc, brewer2mpl
+from brewer2mpl import sequential
+import pandas as pd
+from PIL import Image
+from netCDF4 import Dataset
+import numpy.ma as ma
+import pysal.esda.mapclassify as br
+
+variablePath  = '/home/tmp/nc/'
+#variablePath = '/Users/maria/Downloads/'
+
+cropList = pd.read_csv('/home/django/spam2005-global/data_py/spam_crops.csv')
+#cropList = pd.read_csv('/Users/maria/Projects/spam2005-global/data_py/spam_crops.csv')
+
+logos = Image.open('/home/mcomanescu/logos/spam_logos2.png')
+#logos = Image.open('/Users/maria/Projects/tests/results/spam_logos2.png')
+
+outputFolder = '/home/tmp/png/'
+#outputFolder = '/Users/maria/Downloads/'
+
+def read_data(variableFolder, crop):
+	print crop
+	filename = crop + '.tiff.nc'
+	print variablePath + filename
+	datafile = Dataset(variablePath + variableFolder + '/' + filename)
+	#datafile = Dataset(variablePath + filename)
+
+	data = datafile.variables['Band1'][:]
+	lats = datafile.variables['lat'][:]
+	lons = datafile.variables['lon'][:]
+
+	d2 = ma.masked_where(data <= 1, data)
+	df = pd.DataFrame(d2)
+
+	res = pd.DataFrame()
+	res.lats = lats
+	res.lons = lons
+	res.d2 = d2
+	return res
+
+def get_bins(d2):
+
+	df = pd.DataFrame(d2)
+	s = df.unstack()
+	s_one = s[~np.isnan(s)]
+	if len(s_one) == 0 : return []
+
+	jc = br.Jenks_Caspall(s_one, k = 8)
+
+	for i in range(0,len(jc.bins)):
+		if len(str(int(jc.bins[i]))) >= 3: 
+			jc.bins[i] = np.round(jc.bins[i] / 100.0 ,0) * 100
+
+	return jc.bins
+
+def plot_map(res, bins, bmap, cropName, technologyName, variableName, unitLabel, parentFolder, outputFile):
+	
+	cmap = bmap.get_mpl_colormap(N=256)
+
+	figTitle = cropName + ' ' + technologyName + ' ' + variableName + ' (' + unitLabel + ')' 
+
+	plt.close()
+	plt.figtext(0.025,0.92, figTitle, clip_on = 'True', size = 'large', weight = 'semibold'); 
+	plt.figtext(0.025,0.86, 'Spatially disaggregated production statistics of circa 2005 using the Spatial Production Allocation Model (SPAM).', clip_on = 'True', size = 'x-small', stretch = 'semi-condensed', weight = 'medium');
+	plt.figtext(0.025,0.835,'Values are for 5 arc-minute grid cells.', clip_on = 'True', size = 'x-small', stretch = 'semi-condensed', weight = 'medium');
+
+	plt.figtext(0.025,0.072, 'You, L., U. Wood-Sichra, S. Fritz, Z. Guo, L. See, and J. Koo. 2014', clip_on = 'True', size = 'xx-small', stretch = 'semi-condensed', weight = 'medium')
+	plt.figtext(0.025,0.051, 'Spatial Production Allocation Model (SPAM) 2005 Beta Version.', clip_on = 'True', size = 'xx-small', stretch = 'semi-condensed', weight = 'medium')
+	plt.figtext(0.025,0.030, '08.15.2014. Available from http://mapspam.info', clip_on = 'True', size = 'xx-small', stretch = 'semi-condensed', weight = 'medium');
+
+	plt.figimage(logos,1830, 100)
+
+	#map = Basemap(projection='merc',resolution='i', epsg=4326, lat_0 = 0, lon_0 = 20) # includes all Antarctica
+	map = Basemap(projection='merc',resolution='l', epsg=4326, lat_0 = 0, lon_0 = 20, llcrnrlon=-160, llcrnrlat=-70, urcrnrlon=200, urcrnrlat=90)
+	cs = map.pcolormesh(res.lons, res.lats, res.d2, cmap=cmap, norm=BoundaryNorm(bins, 256, clip=True))
+	map.drawlsmask(land_color='#fafafa', lakes = True)
+	
+	map.drawcountries(linewidth=0.05)
+	map.drawcoastlines(linewidth=0.05)
+
+	cbar = map.colorbar(cs,location='bottom', pad='3%')
+	#cbar.ax.set_xlabel(unitLabel, fontsize = 'x-small'); lebel
+
+	labels = [item.get_text() for item in cbar.ax.get_xticklabels()]
+	labels[0] = '1'; labels[6] = labels[6] + ' <'; labels[7] = ''
+	cbar.ax.set_xticklabels(labels)
+
+	plt.tight_layout(h_pad=0.9, w_pad = 0.9)
+	plt.savefig(outputFolder + parentFolder + '/' + outputFile + '.png', format='png', dpi=400)
+
+for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest'):
+#for crop in ('whea', 'smil'):
+	result = read_data('quickstart_harvested', crop)
+	bins = get_bins(result.d2)
+	if (len(bins) != 0):
+		cropName = cropList[cropList['varCode'] == crop].varName.values[0]
+ 		plot_map(result, bins, sequential.Oranges[7], cropName, 'Total', 'Harvested Area', 'ha', 'quickstart_harvested', crop)
+ 		
+ 		result_i = read_data('quickstart_harvested', crop + '_i')
+ 		plot_map(result_i, bins, sequential.Oranges[7], cropName, 'Irrigated', 'Harvested Area', 'ha', 'quickstart_harvested', crop + '_i')
+ 		
+ 		result_r = read_data('quickstart_harvested', crop + '_r')
+ 		plot_map(result_r, bins, sequential.Oranges[7], cropName, 'Rainfed', 'Harvested Area', 'ha', 'quickstart_harvested', crop + '_r')
+
+for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest'):
+	result = read_data('quickstart_area', crop)
+	bins = get_bins(result.d2)
+	if (len(bins) != 0):
+		cropName = cropList[cropList['varCode'] == crop].varName.values[0]
+ 		plot_map(result, bins, sequential.Oranges[7], cropName, 'Total', 'Physical Area', 'ha', 'quickstart_area', crop)
+ 		
+ 		result_i = read_data('quickstart_area', crop + '_i')
+ 		plot_map(result_i, bins, sequential.Oranges[7], cropName, 'Irrigated', 'Physical Area', 'ha', 'quickstart_area', crop + '_i')
+ 		
+ 		result_r = read_data('quickstart_area', crop + '_r')
+ 		plot_map(result_r, bins, sequential.Oranges[7], cropName, 'Rainfed', 'Physical Area', 'ha', 'quickstart_area', crop + '_r')
+
+for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest'):
+	result = read_data('quickstart_yield', crop)
+	bins = get_bins(result.d2)
+	if (len(bins) != 0):
+		cropName = cropList[cropList['varCode'] == crop].varName.values[0]
+ 		plot_map(result, bins, sequential.YlGnBu[7], cropName, 'Total', 'Yield', 'kg/ha', 'quickstart_yield', crop)
+ 		
+ 		result_i = read_data('quickstart_yield', crop + '_i')
+ 		plot_map(result_i, bins, sequential.YlGnBu[7], cropName, 'Irrigated', 'Yield', 'kg/ha', 'quickstart_yield', crop + '_i')
+ 		
+ 		result_r = read_data('quickstart_yield', crop + '_r')
+ 		plot_map(result_r, bins, sequential.YlGnBu[7], cropName, 'Rainfed', 'Yield', 'kg/ha', 'quickstart_yield', crop + '_r')
+
+for crop in ('whea', 'rice', 'maiz', 'barl', 'pmil', 'smil', 'sorg', 'ocer', 'pota', 'swpo', 'yams', 'cass', 'orts', 'bean', 'chic', 'cowp', 'pige', 'lent', 'opul', 'soyb', 'grou', 'cnut', 'oilp', 'sunf', 'rape', 'sesa', 'ooil', 'sugc', 'sugb', 'cott', 'ofib', 'acof', 'rcof', 'coco', 'teas', 'toba', 'bana', 'plnt', 'trof', 'temf', 'vege', 'rest'):
+	result = read_data('quickstart_prod', crop)
+	bins = get_bins(result.d2)
+	if (len(bins) != 0):
+		cropName = cropList[cropList['varCode'] == crop].varName.values[0]
+ 		plot_map(result, bins, sequential.Greens[7], cropName, 'Total', 'Production', 'mt', 'quickstart_prod', crop)
+ 		
+ 		result_i = read_data('quickstart_prod', crop + '_i')
+ 		plot_map(result_i, bins, sequential.Greens[7], cropName, 'Irrigated', 'Production', 'mt', 'quickstart_prod', crop + '_i')
+ 		
+ 		result_r = read_data('quickstart_prod', crop + '_r')
+ 		plot_map(result_r, bins, sequential.Greens[7], cropName, 'Rainfed', 'Production', 'mt', 'quickstart_prod', crop + '_r')
