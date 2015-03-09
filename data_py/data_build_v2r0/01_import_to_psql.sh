00cc0f49e5a70cccf5b42c3bc4d8627bafa1a54d
mcomanescu <mery.comanescu@gmail.com>
2015-03-09 14:31:25 -0400

    update scripts

--- data_py/data_build_v2r0/01_import_to_psql.sh
+++ data_py/data_build_v2r0/01_import_to_psql.sh
@@ -10,11 +10,13 @@ iconv -f latin1 -t utf-8 spam2005v2r0_physical-area_latin1.csv > spam2005v2r0_ph
 iconv -f latin1 -t utf-8 spam2005v2r0_production_latin1.csv > spam2005v2r0_production.csv
 iconv -f latin1 -t utf-8 spam2005v2r0_yield_latin1.csv > spam2005v2r0_yield.csv
 
+# server
 sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_harvested-area.csv
 sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_yield.csv
 sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_physical-area.csv
 sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_production.csv
 
+# local:
 /Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_harvested-area.csv
 /Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_yield.csv
 /Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_physical-area.csv
