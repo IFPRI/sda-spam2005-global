2caa240fd0ee307a50f2fa677b41f8bb0febc40e
mcomanescu <mery.comanescu@gmail.com>
2015-03-09 13:53:24 -0400

    update scripts: import, add geometry, alter columns

--- data_py/data_build_v2r0/01_import_to_psql.sh
+++ data_py/data_build_v2r0/01_import_to_psql.sh
@@ -1,4 +1,21 @@
-cd /Users/maria/Projects/SPAM2005v2_data_build/20150225/data_in
+cd /Users/maria/Projects/spam2005-global/data_py/data_build_v2r0/data_in
 
-/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"host=localhost user=djuser dbname=djspamdata password=djpass" spam2005v2_yield_chunck.csv
-/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"host=localhost user=djuser dbname=djspamdata password=djpass" spam2005v2_harvested-area_chunck.csv
+mv spam2005v2r0_yield.csv spam2005v2r0_yield_latin1.csv
+mv spam2005v2r0_production.csv spam2005v2r0_production_latin1.csv
+mv spam2005v2r0_physical-area.csv spam2005v2r0_physical-area_latin1.csv
+mv spam2005v2r0_harvested-area.csv spam2005v2r0_harvested-area_latin1.csv
+
+iconv -f latin1 -t utf-8 spam2005v2r0_harvested-area_latin1.csv > spam2005v2r0_harvested-area.csv
+iconv -f latin1 -t utf-8 spam2005v2r0_physical-area_latin1.csv > spam2005v2r0_physical-area.csv
+iconv -f latin1 -t utf-8 spam2005v2r0_production_latin1.csv > spam2005v2r0_production.csv
+iconv -f latin1 -t utf-8 spam2005v2r0_yield_latin1.csv > spam2005v2r0_yield.csv
+
+sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_harvested-area.csv
+sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_yield.csv
+sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_physical-area.csv
+sudo -u postgres ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_production.csv
+
+/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_harvested-area.csv
+/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_yield.csv
+/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_physical-area.csv
+/Applications/Postgres.app/Contents/Versions/9.3/bin/ogr2ogr -f "PostgreSQL" PG:"user=djuser dbname=djspamdata password=djpass" spam2005v2r0_production.csv
--- data_py/data_build_v2r0/02_add_geometry.sql
+++ data_py/data_build_v2r0/02_add_geometry.sql
@@ -1,8 +1,33 @@
 # PSQL
 CREATE INDEX ON hc_grid5m (cell5m);
 
-alter table spam2005v2_yield_chunck alter column cell5m type numeric(19,5) using cell5m::numeric;
-alter table spam2005v2_harvested_area_chunck alter column cell5m  type numeric(19,5) using cell5m::numeric;
+alter table spam2005v2_yield alter column cell5m type numeric(19,5) using cell5m::numeric;
+alter table spam2005v2_harvested_area alter column cell5m  type numeric(19,5) using cell5m::numeric;
+alter table spam2005v2_production alter column cell5m type numeric(19,5) using cell5m::numeric;
+alter table spam2005v2_physical_area alter column cell5m  type numeric(19,5) using cell5m::numeric;
+
+SELECT AddGeometryColumn ('spam2005v2_yield','wkb_geometry',4326,'POLYGON',2);
+SELECT AddGeometryColumn ('spam2005v2_harvested_area','wkb_geometry',4326,'POLYGON',2);
+SELECT AddGeometryColumn ('spam2005v2_production','wkb_geometry',4326,'POLYGON',2);
+SELECT AddGeometryColumn ('spam2005v2_physical_area','wkb_geometry',4326,'POLYGON',2);
+CREATE INDEX spam2005v2_yield_geom_idx ON spam2005v2_yield USING GIST (wkb_geometry);
+CREATE INDEX spam2005v2_harvested_area_geom_idx ON spam2005v2_harvested_area USING GIST (wkb_geometry);
+CREATE INDEX spam2005v2_production_geom_idx ON spam2005v2_production USING GIST (wkb_geometry);
+CREATE INDEX spam2005v2_physical_area_geom_idx ON spam2005v2_physical_area USING GIST (wkb_geometry);
+
+CREATE INDEX ON spam2005v2_yield (cell5m);
+CREATE INDEX ON spam2005v2_harvested_area (cell5m);
+CREATE INDEX ON spam2005v2_production (cell5m);
+CREATE INDEX ON spam2005v2_physical_area (cell5m);
+
+update spam2005v2_yield y set wkb_geometry = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
+update spam2005v2_harvested_area y set wkb_geometry = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
+update spam2005v2_production y set wkb_geometry = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
+update spam2005v2_physical_area y set wkb_geometry = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
+
+
+
+
 
 alter table spam2005v2_yield_chunck alter column maiz type numeric(19,5) using maiz::numeric;
 alter table spam2005v2_harvested_area_chunck alter column maiz  type numeric(19,5) using maiz::numeric;
@@ -15,13 +40,3 @@ alter table spam2005v2_harvested_area_chunck alter column whea type numeric(19,5
 alter table spam2005v2_yield_chunck alter column rice type numeric(19,5) using rice::numeric;
 alter table spam2005v2_harvested_area_chunck alter column rice type numeric(19,5) using rice::numeric;
 
-SELECT AddGeometryColumn ('spam2005v2_yield_chunck','geom',4326,'POLYGON',2);
-SELECT AddGeometryColumn ('spam2005v2_harvested_area_chunck','geom',4326,'POLYGON',2);
-CREATE INDEX spam2005v2_yield_chunck_idx ON spam2005v2_yield_chunck USING GIST (geom);
-CREATE INDEX spam2005v2_harvested_area_chunck_idx ON spam2005v2_harvested_area_chunck USING GIST (geom);
-
-CREATE INDEX ON spam2005v2_yield_chunck (cell5m);
-CREATE INDEX ON spam2005v2_harvested_area_chunck (cell5m);
-
-update spam2005v2_yield_chunck y set geom = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
-update spam2005v2_harvested_area_chunck y set geom = (select h.wkb_geometry from hc_grid5m h where h.cell5m = y.cell5m);
